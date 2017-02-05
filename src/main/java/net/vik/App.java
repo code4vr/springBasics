738d49b5444142b3c062ac678e05abeea04d735c
code4vr <code4vr@gmail.com>
2017-02-05 09:25:45 -0500

    build config

---
+++ src/main/java/net/vik/App.java
@@ -0,0 +1,25 @@
+package net.vik;
+import org.apache.log4j.Logger;
+import org.joda.time.LocalDate;
+/**
+ * Created by Vikram on 2/5/2017.
+ */
+public class App {
+
+    private static final Logger logger = Logger.getLogger(App.class);
+
+    public static void main(String[] args) {
+        System.out.println(getLocalCurrentDate());
+    }
+
+    private static String getLocalCurrentDate() {
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("getLocalCurrentDate() is executed!");
+        }
+
+        LocalDate date = new LocalDate();
+        return date.toString();
+
+    }
+}
